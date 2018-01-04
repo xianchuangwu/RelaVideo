c0bf048a2ccc8624a6f061c3280d2e0b6415d51f
rela_chad <xianchuang.wu@thel.co>
2018-01-04 18:13:07 +0800

    修改项目名称

--- gpuimage/build/intermediates/ndkBuild/release/obj/local/arm64-v8a/objs/gpuimage-library/yuv-decoder.o.d
+++
@@ -1,2 +0,0 @@
-/Users/wuxianchuang/AndroidStudioProjects/rela/rela_android/RelaVideoDemo/gpuimage/build/intermediates/ndkBuild/release/obj/local/arm64-v8a/objs/gpuimage-library/yuv-decoder.o: \
-  /Users/wuxianchuang/AndroidStudioProjects/rela/rela_android/RelaVideoDemo/gpuimage/jni/yuv-decoder.c
--- gpuimage/build/intermediates/ndkBuild/release/obj/local/armeabi-v7a/objs/gpuimage-library/yuv-decoder.o.d
+++
@@ -1,2 +0,0 @@
-/Users/wuxianchuang/AndroidStudioProjects/rela/rela_android/RelaVideoDemo/gpuimage/build/intermediates/ndkBuild/release/obj/local/armeabi-v7a/objs/gpuimage-library/yuv-decoder.o: \
-  /Users/wuxianchuang/AndroidStudioProjects/rela/rela_android/RelaVideoDemo/gpuimage/jni/yuv-decoder.c
